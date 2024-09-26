4029aeb37df585aa7595ecc5b8b1556220c48d86
Vic <161736560+vicvlz1@users.noreply.github.com>
2024-09-26 09:24:01 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+// comédia, romance, drama
+
+// brooklyn-99, LIVRE, comédia, sitcom
+// how i meet you mother, 12, comédia, sitcom
+// greys anatomy, 14, drama médico, romance
+// modern family, LIVRE, comédia, sitcom,clássico
+// o urso, 16, drama, comédia
+// o menino que descobriu o vento, 14, drama
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
