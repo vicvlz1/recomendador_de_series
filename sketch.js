c4d98f1b3b78ae3b6d16753dd79d2b38a801f820
Vic <161736560+vicvlz1@users.noreply.github.com>
2024-10-17 09:19:56 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,13 +5,35 @@
 // greys anatomy, 14, drama médico, romance
 // modern family, LIVRE, comédia, sitcom,clássico
 // o urso, 16, drama, comédia
-// o menino que descobriu o vento, 14, drama
+// the office, 14, comédia, sitcom
 
+let campoIdade;
+let campoDrama;
+let CampoComedida;
 
 function setup() {
-  createCanvas(400, 400);
+  createElement("h2","Recomendador de filmes")
+  createCanvas(800, 400);
+  createSpan("SuaIdade:");
+  campoIdade = createInput("16");
+  campoDrama
 }
 
 function draw() {
   background(220);
+  let idade = campoIdade;value();
+let recomendacao= geraRecomendacao(idade);
+  text(recomendacao,width / 2, height / 2);
 }
+
+function geraRecomendacao(idade) {
+  if(idade>= LIVRE)
+  return "Modern Family"
+
+} 
+}else {
+  return "brookl"
+}
+
+
+
