4dd7adba1af7839dfc51390f35a979190fe8ea6c
Vic <161736560+vicvlz1@users.noreply.github.com>
2024-10-24 11:25:52 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,39 +1,53 @@
-// comédia, romance, drama
-
-// brooklyn-99, LIVRE, comédia, sitcom
-// how i meet you mother, 12, comédia, sitcom
-// greys anatomy, 14, drama médico, romance
-// modern family, LIVRE, comédia, sitcom,clássico
-// o urso, 16, drama, comédia
-// the office, 14, comédia, sitcom
-
 let campoIdade;
+let campoComédia;
 let campoDrama;
-let CampoComedida;
 
 function setup() {
-  createElement("h2","Recomendador de filmes")
   createCanvas(800, 400);
-  createSpan("SuaIdade:");
-  campoIdade = createInput("16");
-  campoDrama
+  createElement("h2", "Recomendador de series");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de comédia?");
+  campoAventura = createCheckbox("Gosta de drama?");
 }
 
 function draw() {
-  background(220);
-  let idade = campoIdade;value();
-let recomendacao= geraRecomendacao(idade);
-  text(recomendacao,width / 2, height / 2);
+  background("white");
+  let idade = campoIdade.value();
+  let gostaDeComédia = campoComédia.checked();
+  let gostaDeDrama = campoDrama.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeComédia, gostaDeDrama);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
 }
 
-function geraRecomendacao(idade) {
-  if(idade>= LIVRE)
-  return "Modern Family"
-
-} 
-}else {
-  return "brookl"
+function geraRecomendacao(idade, gostaDeComédia, gostaDeDrama) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "The officie";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeComédia || gostaDeDrama) {
+          return "modern family";          
+        } else{
+         return "How I Met Your Mother";
+        }
+      } else {
+        if (gostaDeDrama) {
+          return "The Bear";
+        } else {
+          return "Gossip Girl";
+        }
+      }
+    }
+  } else {
+    if (gostaDeComédia) {
+      return "Brooklyn-99";
+    } else {
+      return "O feitiço do tempo";
+    }
+  }
 }
-
-
-
